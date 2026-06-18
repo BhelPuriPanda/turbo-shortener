31ea7338646daf91fb46ff3ac80b6a6785c35556
BhelPuriPanda <boreover582@gmail.com>
2026-06-19 01:07:05 +0530

    day3 : bug fixes env rem

--- src/db.js
+++ src/db.js
@@ -8,29 +8,3 @@ const pool = new pg.Pool({
 });
 
 export default pool;
-
-//IF NOT MADE ALREADY THEN ONLY DELETE AFTER
-export async function setupDatabase() {
-    try {
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS links (
-                id SERIAL PRIMARY KEY,
-                code VARCHAR(10) UNIQUE NOT NULL,
-                original_url TEXT NOT NULL,
-                created_at TIMESTAMP DEFAULT NOW()
-            )
-        `);
-
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS clicks (
-                id SERIAL PRIMARY KEY,
-                code VARCHAR(10) NOT NULL,
-                clicked_at TIMESTAMP DEFAULT NOW()
-            )
-        `);
-        console.log('Database tables ready');
-    } catch (error) {
-        console.error('Database setup failed:', error);
-        throw error;
-    }
-}
